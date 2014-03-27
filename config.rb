d72ff8d3114e458bf27987487994f1f7bb53e7a8
Ian Macalinao <ian@giza.us>
2014-03-27 18:44:25 -0500

    Blog source
    Signed-off-by: Ian Macalinao <ian@giza.us>

--- config.rb
+++ config.rb
@@ -4,11 +4,11 @@ require 'sass-globbing'
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
+http_path = "/simplyian/"
+http_images_path = "/simplyian/images"
 http_generated_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_fonts_path = "/simplyian/fonts"
+css_dir = "public/simplyian/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
